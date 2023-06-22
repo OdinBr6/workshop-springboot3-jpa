3bd86880d828a4d255850b321ac9a6df214490f0
CristianRamos <crisramosrodrigues@hotmail.com>
2023-06-22 17:33:45 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -119,6 +119,13 @@ public class Order implements Serializable{
 		return Objects.hash(id);
 	}
 
+	public Double getTotal() {
+		double sum = 0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
